2b8f763ec25535e0202e50b107491b09f3e7d0be
Roman Dmiterko <dmiterko35@gmail.com>
2015-05-05 00:07:46 +0300

    added method setValue

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
 
-
-division({
+division = new division({
   tags: ['0', '1', '2', '3', '4', '5', '6','7','8','9'],
   divisionSep: 9
 })
-
 
 function polarToDecart(centerX, centerY, radius, angleInDegrees) {
   var angleInRadians = (angleInDegrees-90) * Math.PI / 180;
@@ -16,7 +14,6 @@ function polarToDecart(centerX, centerY, radius, angleInDegrees) {
 }
 
 function drawArc(x, y, radius, startAngle, endAngle){
-
   var start = polarToDecart(x, y, radius, endAngle),
       end = polarToDecart(x, y, radius, startAngle),
       arcSweep = endAngle - startAngle <= 180 ? '0' : '1',
@@ -24,7 +21,6 @@ function drawArc(x, y, radius, startAngle, endAngle){
         'M', start.x, start.y,
         'A', radius, radius, 0, arcSweep, 0, end.x, end.y
       ].join(' ');
-
   return d;
 }
 
@@ -73,17 +69,17 @@ function division(params) {
   gauge.appendChild(gaugeYellow);
   gauge.appendChild(gaugeRed);
 
-  var values = {
+  this.values = {
     tags: [0,1,2,3,4,5,6],
     radius: 160,
     divisionSep: 3,
     arrowWidth: 8,
   };
-  for (a in params) {values[a] = params[a];}
+  for (a in params) {this.values[a] = params[a];}
 
-      radius = values.radius,
-      divisionPoints = values.tags.length,
-      labelsCounter = (divisionPoints-1)*values.divisionSep + divisionPoints;
+      radius = this.values.radius,
+      divisionPoints = this.values.tags.length,
+      labelsCounter = (divisionPoints-1)*this.values.divisionSep + divisionPoints;
       angle = 210,
       step = (4*Math.PI/3) / labelsCounter, // 4/3 - for 240 gauge
       k = 0;
@@ -101,39 +97,37 @@ function division(params) {
   document.getElementById('gauge__grey').setAttribute('d', drawArc(radius, radius, radius-10, -120, 120));
   document.getElementById('gauge__yellow').setAttribute('d', drawArc(radius, radius, radius-10, 60, 90));
   document.getElementById('gauge__red').setAttribute('d', drawArc(radius, radius, radius-10, 90, 120));
-  document.getElementById('gauge__polygon').setAttribute('points', drawArrow(radius, values.arrowWidth));
+  document.getElementById('gauge__polygon').setAttribute('points', drawArrow(radius, this.values.arrowWidth));
 
   var height = division.clientHeight,
       width = division.clientWidth;
+
+  var rotateAngles = [];
 
   for (var i=0; i<labelsCounter; i++) {
     var clone = dot.cloneNode(true);
     var special = false;
 
-    if (i % (values.divisionSep+1) === 0) {
+    if (i % (this.values.divisionSep+1) === 0) {
       clone.className += ' special-dot'
       var tag = document.createElement('span');
       tag.className = 'value';
-      var tagText = document.createTextNode(values.tags[k])
+      var tagText = document.createTextNode(this.values.tags[k])
       tag.appendChild(tagText);
-
       k = k+1;
       clone.appendChild(tag);
-
       special = true;
     }
 
     division.appendChild(clone);
 
 
-    var cloneX = width/2 + radius * Math.cos(angle);
+    var cloneX = width/2 + radius * Math.cos(angle),
         cloneY = height/2 + radius * Math.sin(angle);
-
     var pad = 20;
 
     if (special == true) {
-      rotateAngle = Math.atan( Math.abs(radius + 20 - cloneY)/Math.abs(radius + 20 - cloneX)) *57.3
-
+      rotateAngle = Math.atan( Math.abs(radius + 20 - cloneY)/Math.abs(radius + 20 - cloneX)) *57.3;
       if (cloneX < radius+20) {
         if (cloneY < radius +20 ) {
           rotateAngle = 180+rotateAngle
@@ -146,20 +140,39 @@ function division(params) {
           rotateAngle = -rotateAngle
         }
       }
+
+      rotateAngles.push(rotateAngle);
       clone.style.transform = 'rotate('+rotateAngle+'deg)';
       tag.style.transform = 'rotate('+ -rotateAngle +'deg)';
       tag.style.top = -tag.offsetHeight/2+'px';
     }
-
-
     clone.style.left = cloneX+'px';
     clone.style.top = (cloneY - clone.offsetHeight/2 )+'px';
     angle += step;
-
   }
 
-  specialLabels = document.getElementsByClassName('special-dot');
+  this.setValue = function(type, value) {
+    var entry = (this.values.tags.indexOf(value) != -1)
+    if (type == 'string') {
+      if (entry == true) {
+        var i = this.values.tags.indexOf(value),
+        gaugeArrow.style.transform = 'rotate('+(180+rotateAngles[i])+'deg)';
+      }
+      else if (value >=0 && value <= 360) {
+        value = Number(value);
+        gaugeArrow.style.transform = 'rotate('+ value+'deg)';
+      }
+    }
+    else if (type == 'deg') {
+      gaugeArrow.style.transform = 'rotate('+ value+'deg)';
+    }
+    else {
+      console.log('Value type error')
+    }
+  }
+
 }
 
+division.setValue('deg','0');
 
 
