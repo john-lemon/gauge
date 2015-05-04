ba0bd68cff6cb314c3f0fd755b0d5d6d9e0ea2a3
Roman Dmiterko <dmiterko35@gmail.com>
2015-05-04 17:42:29 +0300

    fixed transform bug in ff and added lables positioning

--- script.js
+++ script.js
@@ -31,51 +31,83 @@ function division(params) {
 
   var values = {
     tags: [0,1,2,3,4,5,6],
-    radius: 180,
+    radius: 160,
     divisionSep: 3
   };
   for (a in params) {values[a] = params[a];}
-
 
   var divisionPoints = values.tags.length,
       division = document.getElementById('division'),
       divisionSpecial = document.getElementById('division-special'),
       label = document.getElementsByClassName('dot')[0],
 
-      radius = 160,
+      radius = values.radius,
 
       labelsCounter = (divisionPoints-1)*values.divisionSep + divisionPoints;
+
       height = division.clientHeight,
       width = division.clientWidth,
-      step = (4*Math.PI/3) / labelsCounter,
       angle = 210,
-      k = 0,
+      step = (4*Math.PI/3) / labelsCounter, // 4/3 - for 240 gauge
+      k = 0;
 
-      console.log(step);
 
   for (var i=0; i<labelsCounter; i++) {
     var clone = label.cloneNode(true);
-
+    var special = false;
     if (i % (values.divisionSep+1) === 0) {
-      clone.className += ' special'
-      var tag = document.createTextNode(values.tags[k]);
+      clone.className += ' special-dot'
+      var tag = document.createElement('span');
+      tag.className = 'value';
+      var tagText = document.createTextNode(values.tags[k])
+      tag.appendChild(tagText);
       k = k+1;
       // clone.style.transform = "rotate("+(angle*i)+"deg)";
       clone.appendChild(tag);
+      special = true;
     }
 
-    var cloneX = Math.round(width/2 + radius * Math.cos(angle) - clone.clientWidth/2);
-        cloneY = Math.round(height/2 + radius * Math.sin(angle) - clone.clientHeight/2);
-    clone.style.left = cloneX+'px';
-    clone.style.top = cloneY+'px';
-
     division.appendChild(clone);
+
+
+    var cloneX = width/2 + radius * Math.cos(angle);
+        cloneY = height/2 + radius * Math.sin(angle);
+
+    var pad = 20;
+
+    if (special == true) {
+      var newCloneX = Math.abs(cloneX - radius - 20);
+      var newCloneY = Math.abs(cloneY - radius - 20);
+      corner = Math.atan(newCloneY/newCloneX);
+      if (cloneX < radius+20) {
+        cloneX -= pad * Math.cos(corner);
+      }
+      else {
+       cloneX += pad * Math.cos(corner);
+      }
+
+      if (cloneY > radius +20 ) {
+        cloneY += pad * Math.sin(corner);
+      }
+      else {
+        cloneY -= pad * Math.sin(corner);
+      }
+    }
+
+    clone.style.width = tag.offsetWidth+'px';
+    clone.style.left = cloneX+'px';
+    clone.style.top = (cloneY - clone.offsetWidth/2 )+'px';
     angle += step;
+
   }
+
+  specialLabels = document.getElementsByClassName('special-dot');
+
+
 }
 
 division({
-  tags: [0,1,2,3, 4, 5, 6],
+  tags: ['0', '1', '2', '3', '4', '5', '6'],
   divisionSep: 9
 })
 
